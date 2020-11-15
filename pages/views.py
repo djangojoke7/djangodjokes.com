555cd5f06eb2d32f9cc367f888f3526b1605f7a9
djangojoke7 <71987197+djangojoke7@users.noreply.github.com>
2020-11-15 10:38:00 +0100

    Delete views.py

--- pages/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-class HomePageView(TemplateView):
-    template_name = 'pages/home.html'
-
-class AboutUsView(TemplateView):
-    template_name = 'pages/about_us.html'
-
